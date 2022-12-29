a183fe2da9297386b89de92eaf348f88cbcbb9f3
Josh Harrison <josh@picklewagon.com>
2022-12-28 18:24:33 -0700

    #3: Document config values

---
+++ config/tradingcardapi.php
@@ -0,0 +1,44 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | API URL
+    |--------------------------------------------------------------------------
+    |
+    | The URL of the API to connect with your application. This value is used
+    | by the client to perform operations on the API.
+    */
+    'url' => env('TRADINGCARDAPI_URL', ''),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Toggle SSL Verification Mode
+    |--------------------------------------------------------------------------
+    |
+    | Toggle SSL verification mode with a boolean value. This usually won't
+    | need to be modified, unless you are working with a local API or any
+    | other API instance without a valid SSL certificate.
+    */
+    'ssl_verify' => (bool) env('TRADINGCARDAPI_SSL_VERIFY', true),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Trading Card API Client ID
+    |--------------------------------------------------------------------------
+    |
+    | The ID of the client used to connect to the API.
+    */
+    'client_id' => env('TRADINGCARDAPI_CLIENT_ID', ''),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Trading Card API Secret
+    |--------------------------------------------------------------------------
+    |
+    | The secret of the client used to connect to the API.
+    */
+    'client_secret' => env('TRADINGCARDAPI_CLIENT_SECRET', ''),
+
+    'cards-connection' => env('CARDS_CONNECTION', 'cards'),
+];
